872867d0893bfe0d98325752c2765ecce10a73df
Juan Carlos Sigler Priego <jcsiglerp@gmail.com>
2018-01-31 12:43:33 -0600

    Randomized quicksort

--- quicksort.py
+++ quicksort.py
@@ -1,3 +1,5 @@
+import random;
+
 def swap(i, j, arr):
 	aux = arr[i];
 	arr[i] = arr[j];
@@ -7,10 +9,13 @@ def quicksort(l, r, arr) :
 	if(l >= r):
 		return;
 	idx = l;
+	swap(l, random.randint(l, r), arr);
 	for i in range(l, r):
 		if arr[i] < arr[r]:
 			swap(i, idx, arr);
 			idx = idx + 1;
+	
+
 	swap(r, idx, arr);
 	quicksort(l, idx - 1, arr);
 	quicksort(idx + 1, r, arr);
