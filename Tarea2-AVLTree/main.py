4919d72c9298bc78bd8ecc1f7271ea0289427a00
Juanito98 <jcsiglerp@gmail.com>
2018-04-11 13:13:41 -0500

    Tarea 2

--- Tarea2-AVLTree/main.py
+++ Tarea2-AVLTree/main.py
@@ -1,5 +1,9 @@
 from AVLTree import *
 
-values = [7, 4, 3, 8, 2, 6, 1, 5]
+values = [100, 300, 400, 350, 375, 50, 200, 360]
 tree = AVLTree(values)
 print(tree)
+for x in [300, 360, 350]:
+    print('Elimina ' + str(x))
+    tree.delete(x)
+    print(tree)
